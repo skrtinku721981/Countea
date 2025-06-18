ffdcef94b59d39ed51406fd52af560b7c4492ed2
Shalish <skrtinku.kumar@gmail.com>
2025-06-18 16:36:19 +0530

    Update db.js and other changes

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb+srv://gaurav:<kzU6RK1Q19IT8dGA>@cluster0.mxkrmkm.mongodb.net/";
+const mongoURI = "mongodb+srv://gaurav:<kzU6RK1Q19IT8dGA>@cluster0.mxkrmkm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const connectToMongo = async () => {
     try {
