1d4fdf39216efb883cd1f5b9a0c952572dbe9504
Shalish <skrtinku.kumar@gmail.com>
2025-06-18 16:32:01 +0530

    Update db.js and other changes

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,13 @@ const express = require('express')
 const app = express()
 
 const cors=require("cors");
-app.use(cors());
+app.use(cors({
+  origin: "https://countea-frontend.onrender.com"
+}));
+//app.use(cors());
 
 connectToMongo();
-const port = 5000
+const port = process.env.PORT || 5000;
 
 
 app.use(express.json())
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
 
   const sendDataToBackend = async (dataArray) => {
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch('https://countea-backend.onrender.com/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
