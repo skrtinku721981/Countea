47553f7cdb423d27c2ead85606a322dd3055876d
Shalish <skrtinku.kumar@gmail.com>
2025-06-23 18:14:27 +0530

    Your commit message

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,29 +3,26 @@ const moment = require('moment-timezone'); // npm install moment-timezone
 const { Schema } = mongoose;
 
 const SnackSchema = new Schema({
-  snack: { type: String, required: true },
-  quantity: { type: Number, required: true }
+  Snack: { type: String, required: true },
+  Quantity: { type: Number, required: true }
 }, { _id: false });
 
 const UserSchema = new Schema({
-  ename: { type: String, required: true },
-  eid: { type: Number, required: true },
-  department: { type: String, required: true },
-  date: { 
-    type: Date, 
-    default: () => moment().tz("Asia/Kolkata").toDate() // IST time
-  },
-  month: { 
+  Ename: { type: String, required: true },
+  Eid: { type: Number, required: true },
+  Department: { type: String, required: true },
+ 
+  Month: { 
     type: String,
     default: () => moment().tz("Asia/Kolkata").format('MMMM') // Full month name
   },
-  dateString: { 
+  Date_Time: { 
     type: String,
      default: () => moment().tz("Asia/Kolkata").format('DD/MM/YYYY HH:mm:ss') // dd/mm/yyyy hh:mm:ss IST
   },
-  snacks: { type: [SnackSchema], required: true },
-  remarks: { type: String, required: true },
-  otherSnack: { type: String }
+  Snacks: { type: [SnackSchema], required: true },
+  Remarks: { type: String, required: true },
+  OtherSnack: { type: String }
 });
 
 module.exports = mongoose.model('user', UserSchema);
